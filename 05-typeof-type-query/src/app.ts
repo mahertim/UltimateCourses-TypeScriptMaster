d7dfc3895cb15ac3e97155f1d9cb720a08e8b6e7
Tim Maher <tim@timmaher.dev>
2019-12-11 10:08:14 -0500

    Type queries

--- 05-typeof-type-query/src/app.ts
+++ 05-typeof-type-query/src/app.ts
@@ -3,12 +3,23 @@ const person = {
   age: 27,
 };
 
+console.log(person);
+console.log(typeof person);
+
 type Person = typeof person;
 
 const anotherPerson: Person = {
   name: 'John',
   age: 30,
+  // cannot add other members
+  // job = 'butler' // does not work
+  // cannot set members as incorrect type
+  // age: '30' // does not work
 };
 
-// JavaScript
-// typeof person // 'object'
+console.log(anotherPerson);
+console.log(typeof anotherPerson);
+
+console.log(typeof '');
+console.log(typeof []);
+console.log(typeof typeof ''); //typeof returns a string
