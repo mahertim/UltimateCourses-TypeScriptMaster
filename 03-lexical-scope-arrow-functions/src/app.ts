a17a0820c48cd8f0bcc17bfb052fbba72143da20
Tim Maher <tim@timmaher.dev>
2019-12-11 09:48:56 -0500

    Arrow functions and lexical scope

--- 03-lexical-scope-arrow-functions/src/app.ts
+++ 03-lexical-scope-arrow-functions/src/app.ts
@@ -1,12 +1,24 @@
 class MyClass {
+  public static foo = 456;
   myMethod() {
     const foo = 123;
+
     console.log('1', this);
+
+    setTimeout(function() {
+      // correct foo, incorrect this
+      console.log('2', this, foo);
+    }, 0);
+
     setTimeout(() => {
-      console.log('2', this);
+      // correct foo, correct this
+      console.log('3', this, foo);
     }, 0);
   }
 }
 
 const myInstance = new MyClass();
 myInstance.myMethod();
+
+// gets printed second due to setTimeout() above
+console.log('4', MyClass.foo);
