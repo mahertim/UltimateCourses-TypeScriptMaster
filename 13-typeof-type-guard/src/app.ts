4ac66aaa3a56726a8be45c7abafeaa83e439620b
Tim Maher <tim@timmaher.dev>
2019-12-11 15:30:31 -0500

    "typeof" and Type guards

--- 13-typeof-type-guard/src/app.ts
+++ 13-typeof-type-guard/src/app.ts
@@ -2,22 +2,24 @@ class Song {
   constructor(public title: string, public duration: string | number) {}
 }
 
-function getSongDuration(item: Song) {
+function getSongDuration(item: Song): string {
   if (typeof item.duration === 'string') {
     return item.duration;
+  } else if (typeof item.duration === 'number') {
+    const { duration } = item;
+    const minutes = Math.floor(duration / 60000);
+    const seconds = (duration / 1000) % 60;
+    return `${minutes}:${seconds}`;
   }
-  const { duration } = item;
-  const minutes = Math.floor(duration / 60000);
-  const seconds = (duration / 1000) % 60;
-  return `${minutes}:${seconds}`;
+  item.duration; // typeof item.duration == 'never' because this line will never be reached
 }
 
 const songDurationFromString = getSongDuration(
-  new Song('Wonderful Wonderful', '05:31')
+  new Song('Wonderful Wonderful', '05:31'),
 );
 console.log(songDurationFromString);
 
 const songDurationFromMS = getSongDuration(
-  new Song('Wonderful Wonderful', 330000)
+  new Song('Wonderful Wonderful', 330000),
 );
 console.log(songDurationFromMS);
