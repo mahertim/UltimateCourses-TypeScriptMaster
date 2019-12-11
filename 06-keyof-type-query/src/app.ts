ec66eacade87c57118b8fd65bfde7499f360f19a
Tim Maher <tim@timmaher.dev>
2019-12-11 10:14:37 -0500

    Index type queries

--- 06-keyof-type-query/src/app.ts
+++ 06-keyof-type-query/src/app.ts
@@ -3,12 +3,15 @@ const person = {
   age: 27,
 };
 
-type Person = typeof person;
-type PersonKeys = keyof Person;
+type Person = typeof person; // {name: string; age: number;}
+type PersonKeys = keyof Person; // "name" | "age"
 
-type PersonTypes = Person[PersonKeys];
+type PersonTypes = Person[PersonKeys]; // string | number
 
 const anotherPerson: Person = {
   name: 'John',
   age: 30,
 };
+
+console.log(person);
+console.log(anotherPerson);
