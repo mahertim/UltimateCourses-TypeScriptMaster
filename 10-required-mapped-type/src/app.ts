5ff794504add107153c7661e1b6788d818aecc43
Tim Maher <tim@timmaher.dev>
2019-12-11 11:23:33 -0500

    Required mapped type, +/- operators

--- 10-required-mapped-type/src/app.ts
+++ 10-required-mapped-type/src/app.ts
@@ -3,8 +3,9 @@ interface Person {
   age?: number;
 }
 
+// available in typescript as Required<T> without defining it ourself
 type MyRequired<T> = {
-  [P in keyof T]-?: T[P]
+  [P in keyof T]-?: T[P];
 };
 
 function printAge(person: Required<Person>) {
@@ -13,7 +14,19 @@ function printAge(person: Required<Person>) {
 
 const person: Required<Person> = {
   name: 'Todd',
-  age: 27
+  age: 27,
 };
 
+console.log(person);
 const age = printAge(person);
+console.log(age);
+
+const person2: Person = {
+  name: 'Tim',
+};
+
+console.log(person2);
+// const age2 = printAge(person2); // does not work because person2 is not a Required<Person>
+person2.age = 25;
+console.log(person2);
+// const age2 = printAge(person2); // still does not work because person2 is still not a Required<Person>
