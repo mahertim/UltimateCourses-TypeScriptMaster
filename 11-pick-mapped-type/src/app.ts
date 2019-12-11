39397dd22836fcbdf2bb7b54b089c9cd6ac511cd
Tim Maher <tim@timmaher.dev>
2019-12-11 11:30:32 -0500

    Pick mapped type

--- 11-pick-mapped-type/src/app.ts
+++ 11-pick-mapped-type/src/app.ts
@@ -1,14 +1,16 @@
 interface Person {
   name: string;
   age: number;
-  address: {}
+  address: {};
 }
 
 type MyPick<T, K extends keyof T> = {
-  [P in K]: T[P]
+  [P in K]: T[P];
 };
 
 const person: Pick<Person, 'name' | 'age'> = {
   name: 'Todd',
-  age: 27
+  age: 27,
 };
+
+console.log(person);
