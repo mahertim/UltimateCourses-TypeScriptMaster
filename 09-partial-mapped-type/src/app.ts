ad6c307dc6e8228363df7c9970f8505bfed2d314
Tim Maher <tim@timmaher.dev>
2019-12-11 11:07:11 -0500

    Partial mapped type

--- 09-partial-mapped-type/src/app.ts
+++ 09-partial-mapped-type/src/app.ts
@@ -4,11 +4,17 @@ interface Person {
 }
 
 type MyPartial<T> = {
-  [P in keyof T]?: T[P]
+  [P in keyof T]?: T[P];
 };
 
+// updates a Person given a partial Person
 function updatePerson(person: Person, prop: Partial<Person>) {
   return { ...person, ...prop };
+}
+
+// updates any object given a partial that shares the type of the object
+function update(t: object, prop: Partial<typeof t>): typeof t {
+  return { ...t, ...prop };
 }
 
 const person: Person = {
@@ -16,4 +22,10 @@ const person: Person = {
   age: 27,
 };
 
-updatePerson(person, { name: 'ABC' });
+console.log(person);
+const newPerson = updatePerson(person, { name: 'ABC' });
+console.log(newPerson);
+
+console.log(person);
+const newerPerson = update(person, { age: 25 });
+console.log(newerPerson);
