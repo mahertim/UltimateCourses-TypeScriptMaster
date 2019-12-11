b7c2fe1b41c7cb1a9605fbe872d64d4aef5138ee
Tim Maher <tim@timmaher.dev>
2019-12-11 14:28:57 -0500

    Record mapped type

--- 12-record-mapped-type/src/app.ts
+++ 12-record-mapped-type/src/app.ts
@@ -10,5 +10,13 @@ const item: Record<keyof TrackStates, string> = {
   next: '8nlksjsk',
 };
 
+const anotherItem: Record<keyof TrackStates, string> = {
+  current: '8nlksjsk',
+  next: '456',
+};
+
 // Numbers are coerced to String
 dictionary[0] = item;
+dictionary[1] = anotherItem;
+
+console.log(dictionary);
