174920ab4bf08d502d8175b84ba553fef108c779
Tim Maher <tim@timmaher.dev>
2019-12-17 10:49:24 -0500

    Function overloads

--- 22-overloads/src/app.ts
+++ 22-overloads/src/app.ts
@@ -1,5 +1,10 @@
+// Define that passing a string causes reverse to return a string
 function reverse(str: string): string;
+
+// Define that passing an array causes reverse to return an array
 function reverse<T>(arr: T[]): T[];
+
+// Implement both reverse functions
 function reverse<T>(stringOrArray: string | T[]): string | T[] {
   if (typeof stringOrArray === 'string') {
     return stringOrArray
@@ -10,5 +15,8 @@ function reverse<T>(stringOrArray: string | T[]): string | T[] {
   return stringOrArray.slice().reverse();
 }
 
-reverse('Pepperoni');
-reverse(['bacon', 'pepperoni', 'chili', 'mushrooms']);
+console.log(reverse('Pepperoni'));
+
+console.log(reverse(['bacon', 'pepperoni', 'chili', 'mushrooms']));
+
+console.log(reverse([1, 2, 3, 4]));
--- 22-overloads/webpack.config.js
+++ 22-overloads/webpack.config.js
@@ -13,4 +13,5 @@ module.exports = {
   devServer: {
     port: 3000,
   },
+  devtool: 'inline-source-map',
 };
